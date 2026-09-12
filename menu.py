2fda70157dca0e5fe9027eba088620a090bfe683
tagoui <tagouli3@gmail.com>
2026-09-12 02:18:14 +0100

    CLI V1

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
 def song_menu():
     print(
-        "[p] Pause/Resume | [s] Save to playlist | [a] Save artist to favorites list |[q] Quit"
+        " [p] Pause/Resume | [s] Save to playlist | [a] Save artist to favorites list | [q] Quit"
     )
